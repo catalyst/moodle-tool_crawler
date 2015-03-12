1ee809ef1b195b97ad03ac2507e333f360a19188
Brendan Heywood <brendan@catalyst-au.net>
2015-03-12 13:14:30 +1100

    MOODLE-730: Detect and handle non utf8 strings

--- classes/robot/crawler.php
+++ classes/robot/crawler.php
@@ -293,6 +293,22 @@ class crawler {
 
         }
 
+        function detectUTF8($string) {
+                return preg_match('%(?:
+                [\xC2-\xDF][\x80-\xBF]        # non-overlong 2-byte
+                |\xE0[\xA0-\xBF][\x80-\xBF]               # excluding overlongs
+                |[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}      # straight 3-byte
+                |\xED[\x80-\x9F][\x80-\xBF]               # excluding surrogates
+                |\xF0[\x90-\xBF][\x80-\xBF]{2}    # planes 1-3
+                |[\xF1-\xF3][\x80-\xBF]{3}                  # planes 4-15
+                |\xF4[\x80-\x8F][\x80-\xBF]{2}    # plane 16
+                )+%xs', $string);
+        }
+
+        if (!detectUTF8($result->title)) {
+            $result->title = utf8_decode($result->title);
+        }
+
         // Wait until we've finished processing the links before we save.
         $DB->update_record('linkchecker_url', $result);
 
