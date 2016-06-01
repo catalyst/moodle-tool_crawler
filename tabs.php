c0bafe09d7397a983f89224a0c6399f8adaff368
Brendan Heywood <brendan@catalyst-au.net>
2016-06-01 12:11:13 +1000

    Better tab logic

--- tabs.php
+++ tabs.php
@@ -30,28 +30,35 @@ if (isset($courseid) && $courseid) {
         if ($rpt != 'queued') {
             $tabs .= ' | ';
         }
-        if (isset($report) && $report == $rpt) {
-            $tabs .= '<b>' . html_writer::link(new moodle_url('report.php', array('report' => $rpt, 'course' => $courseid )),
-                get_string($rpt, 'local_linkchecker_robot')) . '</b>';
-        } else {
-            $tabs .= html_writer::link(new moodle_url('report.php', array('report' => $rpt, 'course' => $courseid )),
+        $wrap = (isset($report) && $report == $rpt) ? 'b' : 'span';
+        $tabs .= html_writer::start_tag($wrap);
+        $tabs .= html_writer::link(new moodle_url('report.php', array('report' => $rpt, 'course' => $courseid )),
                 get_string($rpt, 'local_linkchecker_robot'));
-        }
+        $tabs .= html_writer::end_tag($wrap);
     }
 } else {
-    $tabs .= html_writer::link("/admin/settings.php?section=local_linkchecker_robot",
+
+    $section = optional_param('section', '', PARAM_RAW);
+    $wrap = ($section == 'local_linkchecker_robot') ? 'b' : 'span';
+    $tabs .= html_writer::start_tag($wrap);
+    $tabs .= html_writer::link(new moodle_url("/admin/settings.php?section=local_linkchecker_robot"),
         get_string('settings', 'local_linkchecker_robot'));
+    $tabs .= html_writer::end_tag($wrap);
+
     $tabs .= ' | ';
-    $tabs .= html_writer::link("index.php", get_string('status', 'local_linkchecker_robot'));
+
+    $wrap = 'span';
+    $tabs .= html_writer::start_tag($wrap);
+    $tabs .= html_writer::link(new moodle_url("/local/linkchecker_robot/index.php"), get_string('status', 'local_linkchecker_robot'));
+    $tabs .= html_writer::end_tag($wrap);
+
     foreach ($reports as $rpt) {
         $tabs .= ' | ';
-        if (isset($report) && $report == $rpt) {
-            $tabs .= '<b>' . html_writer::link(new moodle_url('report.php', array('report' => $rpt )),
-                get_string($rpt, 'local_linkchecker_robot')) . '</b>';
-        } else {
-            $tabs .= html_writer::link(new moodle_url('report.php', array('report' => $rpt )),
+        $wrap = (isset($report) && $report == $rpt) ? 'b' : 'span';
+        $tabs .= html_writer::start_tag($wrap);
+        $tabs .= html_writer::link(new moodle_url('report.php', array('report' => $rpt )),
                 get_string($rpt, 'local_linkchecker_robot'));
-        }
+        $tabs .= html_writer::end_tag($wrap);
     }
 }
 $tabs .= '</p>';
