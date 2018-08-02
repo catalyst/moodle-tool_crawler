e38927eced71070138ca7b460d75da82920e87b3
Kristian Ringer <kristian.ringer@catalyst-au.net>
2018-08-02 17:07:19 +1000

    Add retry all button to add all broken links back to queue in one click

--- report.php
+++ report.php
@@ -32,8 +32,9 @@ require_login(null, false);
 $report     = optional_param('report', '', PARAM_ALPHANUMEXT);
 $page       = optional_param('page', 0, PARAM_INT);
 $perpage    = optional_param('perpage', 50, PARAM_INT);
-$courseid   = optional_param('course', 0, PARAM_INT);
-$retryid    = optional_param('retryid', 0, PARAM_INT);
+$courseid   = optional_param('course',  0,  PARAM_INT);
+$retryid    = optional_param('retryid', 0,  PARAM_INT);
+$retryall    = optional_param('retryall', 0,  PARAM_BOOL);
 $start = $page * $perpage;
 
 $sqlfilter = '';
@@ -69,7 +70,11 @@ if ($courseid) {
     require_capability('moodle/site:config', context_system::instance());
     admin_externalpage_setup('tool_crawler_' . $report);
 }
-echo $OUTPUT->header();
+
+// Do not start output if we are redirecting back to this page.
+if ($retryall == 0) {
+    echo $OUTPUT->header();
+}
 
 require('tabs.php');
 echo $tabs;
@@ -138,6 +143,13 @@ if ($report == 'broken' || $report == 'reference') {
     }
     $table->data = [];
     foreach ($data as $row) {
+        if ($retryall == 1) {
+            $retryid = $row->toid;
+            $robot = new \tool_crawler\robot\crawler();
+            $robot->reset_for_recrawl($retryid);
+            continue;
+        }
+
         $text = trim($row->text);
         if ($text == "") {
             $text = get_string('missing', 'tool_crawler');
@@ -164,6 +176,10 @@ if ($report == 'broken' || $report == 'reference') {
             array_push($data, html_writer::link('/course/view.php?id=' . $row->courseid, $escapedshortname));
         }
         $table->data[] = $data;
+    }
+    if ($retryall == 1) {
+        $url = $PAGE->url;
+        redirect($url->raw_out(true), get_string('retryallmessage', 'tool_crawler'));
     }
 } else if ($report == 'queued') {
     $sql = " FROM {tool_crawler_url} a
@@ -371,6 +387,10 @@ echo $OUTPUT->heading(get_string(
     ]
 ));
 echo get_string($report . '_header', 'tool_crawler');
+if (count($data) > 1) {
+    echo html_writer::link(new moodle_url($navurl->out(), array('retryall' => 1 )),
+                           get_string('retryall', 'tool_crawler'));
+}
 echo html_writer::table($table);
 echo $OUTPUT->paging_bar($count, $page, $perpage, $baseurl);
 echo $OUTPUT->footer();
