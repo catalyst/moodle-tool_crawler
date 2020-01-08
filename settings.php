d9a28163c1f3360de2e82a7c78f2e8cfb036c76d
Kristian Ringer <kristian.ringer@catalyst-au.net>
2020-01-08 11:31:22 +1100

    Include constants file in settings.php

--- settings.php
+++ settings.php
@@ -23,6 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die;
+require_once($CFG->dirroot . '/admin/tool/crawler/constants.php');
 
 if ($hassiteconfig) {
 
