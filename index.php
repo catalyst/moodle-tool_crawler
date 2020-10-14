412b436e6322dbfcac16449f3ef18b8426b60e3f
James Williams <jameswilliams@catalyst-au.net>
2020-10-14 13:17:41 +1100

    Suppress warnings on Status page if configured robot user's id/username doesn't exist yet or is renamed to a username that doesn't exist.

--- index.php
+++ index.php
@@ -71,6 +71,11 @@ $duration = time() - $crawlstart;
 $eta = floor($duration / $progress + $crawlstart);
 
 $robot = $DB->get_record('user', ['username' => $config->botusername]);
+if (!$robot) {
+    $robot = new stdClass();
+    $robot->id = get_string('botusermissing', 'tool_crawler');
+    $robot->username = get_string('botusermissing', 'tool_crawler');
+}
 
 $table = new html_table();
 $table->head = [get_string('robotstatus', 'tool_crawler')];
